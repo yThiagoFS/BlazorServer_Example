17e6082e4b7c29e091bde9185d8b55e4f0809a83
Thiago Silva <thi.ferreira.silva03@gmail.com>
2023-10-14 18:07:53 -0300

    crud complete

---
+++ BlazorWASMExample/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorWASMExample.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+
+        }
+
+        public Category(long id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public long Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ BlazorWASMExample/SeedData.cs
@@ -0,0 +1,13 @@
+﻿using BlazorWASMExample.Models;
+
+namespace BlazorWASMExample
+{
+    public static class SeedData
+    {
+        public static List<Category> SeedCategories()
+        => Enumerable.Range(0, 10)
+            .Select(indice => new Category((long)indice, $"Category {indice}", $"Description {indice}"))
+            .ToList();         
+            
+    }
+}
